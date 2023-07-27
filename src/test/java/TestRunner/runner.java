573187c1cfb4cbaf644b727b46ab57d0c169266e
Viru <Viru@192.168.0.5>
2023-07-27 14:27:30 -0400

    commit

--- src/test/java/TestRunner/runner.java
+++ src/test/java/TestRunner/runner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.CucumberOptions;
 //@RunWith(Cucumber.class) //Junit execution
 
 	@CucumberOptions(
-			plugin = {"pretty", "html:target/Login.html"}, //reporting purpose
+			plugin = {"pretty", "html:target/Login.html","json:target/cucumber.json"}, //reporting purpose
 			monochrome=false,  //console output color
 			tags = "", //tags from feature file
 			features = {"src/test/resources/featureFiles"}, //location of feature files
